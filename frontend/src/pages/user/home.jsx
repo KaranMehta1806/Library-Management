3d2f780ceba195af07d46ab01983ef834658f57c
karan <karan8872@gmail.com>
2025-06-13 19:20:26 +0530

    home

--- frontend/src/pages/user/home.jsx
+++ frontend/src/pages/user/home.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import "./home.css";
 import { Link } from "react-router-dom";
 import { FiBook, FiSearch, FiClock, FiUser, FiCalendar } from "react-icons/fi";
+import Preloader from "../../components/Preloader";
 
 export default function Home() {
   const [categories, setCategories] = useState([]);
@@ -59,7 +60,7 @@ export default function Home() {
     fetchData();
   }, []);
 
-    
+   if (loading) return <Preloader />;  
 
   return (
     <div className="library-homepage">
